b7a5e5ca55c51126ea8b7875100216505b0a3c42
uguser35 <uguser35@uguser35-HP8200EliteCX.(none)>
2013-02-26 16:01:17 +0530

    basics of php done!

--- phptest/index.php
+++ phptest/index.php
@@ -16,20 +16,21 @@
 <?php
         //variables galore
         $name = "Siddhartha";
-        $surname = "RT";
+        $middlename = "R";
+        $lastname = "Thota";
         $time = "afternoon";
         //now let's form a sentence
         $statement = "Good ";
         $statement .= $time;
-        $statement .= ", My name is ";
-        $statement .= $surname;
+        $statement .= ", my name is ";
+        $statement .= $lastname;
         //yes, you have to put the spaces yourself.
         $statement .= ", ";
-        $statement .= $name;
+        $statement .= $name." ".$middlename;
         //sample echo statement
-        echo $statement;
+        echo "I'm echoing this: ".$statement;
         //sample print statement
-        print ("<br />i'm printing this: ".$statement);
+        print ("<br />I'm printing this: ".$statement);
         //A comma doesn't work.
 ?>
         </div>
@@ -39,7 +40,7 @@
         $one = 50;
         $two = 60;
         $three = $two/$one;
-        echo $three;
+        echo "50/60 equals ".$three;
 ?>
         </div>
 
@@ -62,31 +63,99 @@
 <?php
         $student_info = array(
             0 => array("name" => "Siddhartha",
-            "age"  => "21",
-            "dept" => "IT",
-            "sex" => "Male"),
+                        "age"  => "21",
+                        "dept" => "IT",
+                        "sex" => "Male",
+                        "pass" => "password",
+                        "hash" => ""),
             1 => array("name" => "Anirudha",
-            "age"  => "21",
-            "dept" => "IT",
-            "sex" => "Male"),
+                        "age"  => "21",
+                        "dept" => "IT",
+                        "sex" => "Male",
+                        "pass" => "s&y",
+                        "hash" => "",),
+            2 => array("name" => "Anirudha",
+                        "age"  => "21",
+                        "dept" => "IT",
+                        "sex" => "Male",
+                        "pass" => "newpass2**",
+                        "hash" => "",),
+            3 => array("name" => "Sagar",
+                        "age"  => "21",
+                        "dept" => "IT",
+                        "sex" => "Male",
+                        "pass" => "awesome$@##word",
+                        "hash" => "",),
         );
+        foreach($student_info as $id=>$student){
+            //$student['hash']= hash("md5", $student['pass']);
+            $student_info[$id]['hash'] =  hash("md5", $student['pass']);
+
+        }
 ?>
         <table>
-            <th><td>Name</td><td>Age</td><td>dept</td><td>sex</td></th>
-<?php
-        for($i=0;$i<count($student_info);$i++){
-            echo "<tr>
-                <td>$student_info[$i]['name']</td>
-                <td>$student_info[$i]['age'] </td>
-                <td>$student_info[$i]['dept'] </td>
-                <td>$student_info[$i]['sex'] </td>
-                </tr>";
-        }
-
-?>
+            <thead><td>Name</td><td>Age</td><td>dept</td><td>sex</td><td>password</td><td>hash</td></thead>
+            <tbody>
+<?php foreach($student_info as $student){ ?>
+            <tr> <?php foreach ($student as $key => $value ){ ?>
+                <td> <?php echo $value ?> </td>
+                <?php } ?>
+            </tr>
+<?php }?>
+</tbody>
         </table>
         </div>
-
+        
+        <div class="exercise">
+            Checking for duplicates:<br />
+            <?php
+                $count = 0;
+                foreach($student_info as $id => $student){
+                    foreach($student_info as $anotherid => $anotherstudent){
+                        if($student['name']==$anotherstudent['name'] && $id != $anotherid ){
+                            $studentname = $student['name'];
+                            $count += 1;
+                            echo "<span class='err'>Error!</span> $studentname repeated<br/>";
+                        }
+                    }
+                }
+                if(!$count){
+                    echo "No errors found.<br />";
+                }
+            ?>
+            <br />Checking passwords:<br/>
+            <?php 
+                foreach($student_info as $student){
+                    $name = $student['name'];
+                    if(ctype_alnum($student['pass'])){
+                        echo "<span class='err'>Error! </span>$name, your password must contain a special character.<br />";
+                    }
+                    if(strlen($student['pass'])<5){
+                        echo "<span class='err'>Error! </span>$name, your password must be atleast 5 chars long <br />";
+                    } 
+                }
+            ?>
+        </div>
+        <div class="exercise">
+            Using get_name_from_ID($array, $item):<br />
+            <?php 
+                function get_name_from_ID($student_info, $q_id){
+                    foreach($student_info as $id => $student ){
+                        if($id == $q_id){
+                           return $student['name']; 
+                        }
+                    }
+                }
+                foreach($student_info as $id=>$student){
+                    echo "id ".$id." is ".get_name_from_ID($student_info, $id)."<br />";
+                }
+            ?>
+        </div>
+        <div class="exercise">
+            Today is :
+            <?php echo date("l, d M Y H:i:s");
+            ?>
+        </div>
     </div>
     </body>
 
